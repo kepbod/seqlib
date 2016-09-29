eea0e49b380241807cb0120fdddf4f2aa2546918
Xiao-Ou Zhang <kepbod@gmail.com>
2016-09-28 21:38:23 -0400

    add more utils

---
+++ utils/parser.py
@@ -0,0 +1,243 @@
+import sys
+
+
+class Info(object):
+
+    def __init__(self, info, type='ref'):
+        if type == 'ref':
+            assert len(info) == 11, 'REF format should have 11 columns'
+        elif type == 'bed':
+            assert len(info) == 12, 'BED format should have 12 columns'
+        else:
+            sys.exit('Wrong format!')
+        self._info = info
+        self._type = type
+
+    @property
+    def gene(self):
+        '''
+        Return gene symbol.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.gene
+        'DDX11L1'
+        '''
+        if self._type == 'ref':
+            return self._info[0]
+        else:
+            return self._info[3]
+
+    @property
+    def isoform(self):
+        '''
+        Return isoform symbol.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.isoform
+        'uc010nxq.1'
+        '''
+        if self._type == 'ref':
+            return self._info[1]
+        else:
+            return self._info[3]
+
+    @property
+    def chrom(self):
+        '''
+        Return chromosome.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.chrom
+        'chr1'
+        '''
+        if self._type == 'ref':
+            return self._info[2]
+        else:
+            return self._info[0]
+
+    @property
+    def strand(self):
+        '''
+        Return strand.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.strand
+        '+'
+        '''
+        if self._type == 'ref':
+            return self._info[3]
+        else:
+            return self._info[5]
+
+    @property
+    def tx_start(self):
+        '''
+        Return transcript start.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.tx_start
+        11873
+        '''
+        if self._type == 'ref':
+            return int(self._info[4])
+        else:
+            return int(self._info[1])
+
+    @property
+    def tx_end(self):
+        '''
+        Return transcript end.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.tx_end
+        14409
+        '''
+        if self._type == 'ref':
+            return int(self._info[5])
+        else:
+            return int(self._info[2])
+
+    @property
+    def total_length(self):
+        '''
+        Return total length (including exons and introns).
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.total_length
+        2536
+        '''
+        return self.tx_end - self.tx_start
+
+    @property
+    def cds_start(self):
+        '''
+        Return cds start.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.cds_start
+        12189
+        '''
+        return int(self._info[6])
+
+    @property
+    def cds_end(self):
+        '''
+        Return cds end.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.cds_end
+        13639
+        '''
+        return int(self._info[7])
+
+    @property
+    def exon_num(self):
+        '''
+        Return exon number.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.exon_num
+        3
+        '''
+        if self._type == 'ref':
+            return int(self._info[8])
+        else:
+            return int(self._info[9])
+
+    @property
+    def intron_num(self):
+        '''
+        Return intron number.
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.intron_num
+        2
+        '''
+        return self.exon_num - 1
+
+    @property
+    def exon_starts(self):
+        '''
+        Return exon starts
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.exon_starts
+        [11873, 12594, 13402]
+        '''
+        if self._type == 'ref':
+            return [int(x) for x in self._info[9].split(',')[:-1]]
+        else:
+            offsets = [int(x) for x in self._info[11].split(',')]
+            return [self.tx_start + x for x in offsets]
+
+    @property
+    def exon_ends(self):
+        '''
+        Return exon ends
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.exon_ends
+        [12227, 12721, 14409]
+        '''
+        if self._type == 'ref':
+            return [int(x) for x in self._info[10].split(',')[:-1]]
+        else:
+            sizes = [int(x) for x in self._info[9].split(',')]
+            return [start + size for start, size in zip(self.exon_starts, sizes)]
+
+    @property
+    def exon_lengths(self):
+        '''
+        Return exon lengths
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.exon_lengths
+        [354, 127, 1007]
+        '''
+        if self._type == 'ref':
+            return [end - start for start, end in zip(self.exon_starts,
+                                                      self.exon_ends)]
+        else:
+            return [int(x) for x in self._info[9].split(',')]
+
+    @property
+    def intron_starts(self):
+        '''
+        Return intron starts
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.intron_starts
+        [12227, 12721]
+        '''
+        return self.exon_ends[:-1]
+
+    @property
+    def intron_ends(self):
+        '''
+        Return intron ends
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.intron_ends
+        [12594, 13402]
+        '''
+        return self.exon_starts[1:]
+
+    @property
+    def intron_lengths(self):
+        '''
+        Return intron lengths
+        >>> info = Info(['DDX11L1', 'uc010nxq.1', 'chr1', '+', '11873', '14409', '12189', '13639', '3', '11873,12594,13402,', '12227,12721,14409,'])
+        >>> info.intron_lengths
+        [367, 681]
+        '''
+        return [end - start for start, end in zip(self.intron_starts,
+                                                  self.intron_ends)]
+
+
+class Annotation(object):
+
+    def __init__(self, fname, type='ref'):
+        self.fh = open(fname, 'r')
+        self.type = type
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        info = self.fh.readline()
+        if info:
+            return Info(info.split(), type=self.type)
+        else:
+            raise StopIteration()
+
+    def close(self):
+        self.fh.close()
+
+
+if __name__ == '__main__':
+      import doctest
+      doctest.testmod()
---
+++ utils/seq.py
@@ -0,0 +1,39 @@
+import sys
+from string import maketrans
+
+
+def dna_to_rna(fa, strand='+', tou=False):
+    '''
+    Convert DNA to RNA.
+    1. convert all the characters to uppercase
+    2. reverse complement for minus strand RNAs
+    3. convert T to U if tou is True
+    >>> dna_to_rna('AAttcgGTA')
+    'AATTCGGTA'
+    >>> dna_to_rna('AAttcgGTA', tou=True)
+    'AAUUCGGUA'
+    >>> dna_to_rna('AAttcgGTA', strand='-')
+    'TACCGAATT'
+    >>> dna_to_rna('AAttcgGTA', strand='-', tou=True)
+    'UACCGAAUU'
+    '''
+    if strand == '+':
+        if tou:
+            table = maketrans('ATCGatcg', 'AUCGaucg')
+            return fa.translate(table).upper()
+        else:
+            return fa.upper()
+    elif strand == '-':
+        if tou:
+            table = maketrans('ATCGatcg', 'UAGCuagc')
+            return fa.translate(table).upper()[::-1]
+        else:
+            table = maketrans('ATCGatcg', 'TAGCtagc')
+            return fa.translate(table).upper()[::-1]
+    else:
+        sys.exit('Strand should be "+" or "-"!')
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
