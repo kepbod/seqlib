86f96471c9b9bc38f2c55174576524aaaf31e8cc
Xiao-Ou Zhang <kepbod@gmail.com>
2016-11-07 15:13:30 -0500

    add bam_to_bedgraph

--- seqlib/ngs.py
+++ seqlib/ngs.py
@@ -59,12 +59,9 @@ def check_bed(bed):
 
 def fetch_juncfile(bam, url=False, dir=None, stranded=False, min=0):
     '''
-    fetch junction reads to create a junc file
+    Fetch junction reads to create a junc file
     '''
-    if url:  # from remote server
-        bamf = pysam.AlignmentFile(bam, 'rb')
-    else:  # from local file
-        bamf = check_bam(bam)
+    bamf = pysam.AlignmentFile(bam, 'rb')
     prefix = os.path.splitext(os.path.split(bam)[-1])[0]
     if not dir:
         if url:
@@ -107,6 +104,66 @@ def fetch_juncfile(bam, url=False, dir=None, stranded=False, min=0):
         sorted_junc_bed = pybedtools.BedTool(tmp.name).sort()
         sorted_junc_bed.saveas(junc_path)
     return junc_path
+
+
+def bam_to_bedgraph(bam, url=False, dir=None, stranded=False, scale=False):
+    '''
+    Convert bam file to bedgraph file
+    Note: `scale` only supports local files
+    '''
+    if url:  # from remote server
+        bamf = pybedtools.BedTool(bam, remote=True)
+    else:  # from local file
+        bamf = pybedtools.BedTool(bam, remote=False)
+        bam_head = check_bam(bam)
+    prefix = os.path.splitext(os.path.split(bam)[-1])[0]
+    if not dir:
+        if url:
+            dir = os.getcwd()
+        else:
+            dir = os.path.dirname(os.path.abspath(bam))
+    else:
+        if not os.path.isdir(dir):
+            sys.exit('Your directory is wrong: %s' % dir)
+    chrom_size_f = tempfile.NamedTemporaryFile('w+')
+    for seq in bam_head.header['SQ']:
+        chrom_size_f.write('%s\t%s\n' % (seq['SN'], seq['LN']))
+    chrom_size_f.seek(0)
+    chrom_size_fn = chrom_size_f.name
+    if scale and not url:
+        mapped_reads = bam_head.mapped
+        for read in bam_head:
+            read_length = read.query_length
+            break
+        s = 1000000000.0 / mapped_reads / read_length
+    else:
+        s = 1
+    if stranded:
+        bedgraph_pfn = os.path.join(dir, prefix + '_plusS.bg')
+        bedgraph_mfn = os.path.join(dir, prefix + '_minusS.bg')
+        _cal_cov(bedgraph_pfn, bamf, chrom_size_fn, s, strand='+')
+        _cal_cov(bedgraph_mfn, bamf, chrom_size_fn, s, strand='-')
+    else:
+        bedgraph_fn = os.path.join(dir, prefix + '.bg')
+        _cal_cov(bedgraph_fn, bamf, chrom_size_fn, s)
+    chrom_size_f.close()
+    if stranded:
+        return (bedgraph_pfn, bedgraph_mfn)
+    else:
+        return bedgraph_fn
+
+
+def _cal_cov(bedgraph_fn, bamf, chrom_size_fn, scale, strand=None):
+    opt = {'bg': True, 'g': chrom_size_fn, 'split': True, 'scale': scale}
+    if strand == '+':
+        opt['strand'] = '-'  # for TruSeq kit
+    elif strand == '-':
+        opt['strand'] = '+'  # for TruSeq kit
+        opt['scale'] *= -1
+    with open(bedgraph_fn, 'w') as bedgraph_f:
+        for line in bamf.genome_coverage(**opt):
+            value = str(int(float(line[3]) + 0.5))
+            bedgraph_f.write('\t'.join(line[:3]) + '\t%s\n' % value)
 
 
 if __name__ == '__main__':
