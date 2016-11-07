58eba0239467f3e99bc18486a0a4cb4f4297c885
Xiao-Ou Zhang <kepbod@gmail.com>
2016-11-07 16:04:27 -0500

    add run_command

--- seqlib/helper.py
+++ seqlib/helper.py
@@ -3,6 +3,7 @@ Some help functions and classes
 '''
 
 import sys
+import os
 
 __author__ = 'Xiao-Ou Zhang <kepbod@gmail.com>'
 
@@ -13,6 +14,12 @@ def check_option(n, msg):
     '''
     if len(sys.argv) != n:
         sys.exit(msg)
+
+
+def run_command(command, message):
+    return_code = os.system(command) >> 8
+    if return_code:
+        sys.exit(message)
 
 
 if __name__ == '__main__':
--- tests/test_ngs.py
+++ tests/test_ngs.py
@@ -4,7 +4,7 @@ Testing parse.py
 
 import os
 from tests.help_test import check_file
-from seqlib.ngs import fetch_juncfile
+from seqlib.ngs import fetch_juncfile, bam_to_bedgraph
 
 
 def test_fetch_juncfile():
@@ -20,3 +20,20 @@ def test_fetch_juncfile():
     junc = fetch_juncfile('data/junc.bam', min=5)
     check_file(junc, 'data/junc_min.bed')
     os.remove(junc)
+
+
+def test_bam_to_bedgraph():
+    '''
+    Testing bam_to_bedgraph()
+    '''
+    bg = bam_to_bedgraph('data/test.bam')
+    check_file(bg, 'data/unstranded.bg')
+    os.remove(bg)
+    bg = bam_to_bedgraph('data/test.bam', scale=True)
+    check_file(bg, 'data/unstranded_scale.bg')
+    os.remove(bg)
+    pbg, mbg = bam_to_bedgraph('data/test.bam', stranded=True)
+    check_file(pbg, 'data/plusS.bg')
+    os.remove(pbg)
+    check_file(mbg, 'data/minusS.bg')
+    os.remove(mbg)
