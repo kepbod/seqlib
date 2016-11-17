b33bb83de9e1263d1b5ad4e46d8aaeb34b19a8fb
Xiao-Ou Zhang <kepbod@gmail.com>
2016-11-17 00:06:17 -0500

    fix bugs

--- seqlib/tests/conftest.py
+++ seqlib/tests/conftest.py
@@ -1,7 +1,30 @@
+import os
 import os.path
 import pytest
 
+pytest_plugins = ['helpers_namespace']
 
-@pytest.fixture
-def data_folder():
-    return os.path.abspath(os.path.dirname(__file__))
+
+@pytest.helpers.register
+def data_path(fn):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), fn)
+
+
+@pytest.helpers.register
+def check_file(test_file, result_file):
+    '''
+    Check if files are existed and same
+    '''
+    print('Check %s file...' % test_file)
+    assert os.path.isfile(test_file), 'No %s file' % test_file
+    assert compare_file(test_file, result_file), 'Difference in %s' % test_file
+
+
+def compare_file(fn1, fn2):
+    '''
+    Test if two files are same
+    '''
+    with open(fn1, 'r') as f1, open(fn2, 'r') as f2:
+        content1 = set(f1.readlines())
+        content2 = set(f2.readlines())
+        return content1 == content2
--- seqlib/tests/test_ngs.py
+++ seqlib/tests/test_ngs.py
@@ -4,27 +4,25 @@ Testing parse.py
 
 import os
 import os.path
-from tests.utils import check_file
+from pytest.helper import data_path, check_file
 from seqlib.ngs import fetch_juncfile, bam_to_bedgraph
 
 
-def test_fetch_juncfile(data_folder):
+def test_fetch_juncfile():
     '''
     Testing fetch_juncfile()
     '''
-    junc = fetch_juncfile(os.path.join(data_folder, 'data/junc.bam'))
-    check_file(junc, os.path.join(data_folder, 'data/junc_unstranded.bed'))
+    junc = fetch_juncfile(data_path('junc.bam'))
+    check_file(junc, data_path('junc_unstranded.bed'))
     os.remove(junc)
-    junc = fetch_juncfile(os.path.join(data_folder, 'data/junc.bam'),
-                          stranded=True)
-    check_file(junc, os.path.join(data_folder, 'data/junc_stranded.bed'))
+    junc = fetch_juncfile(data_path('junc.bam'), stranded=True)
+    check_file(junc, data_path('junc_stranded.bed'))
     os.remove(junc)
-    junc = fetch_juncfile(os.path.join(data_folder, 'data/junc.bam'), min=5)
-    check_file(junc, os.path.join(data_folder, 'data/junc_min.bed'))
+    junc = fetch_juncfile(data_path('junc.bam'), min=5)
+    check_file(junc, data_path('junc_min.bed'))
     os.remove(junc)
-    junc = fetch_juncfile(os.path.join(data_folder, 'data/junc.bam'),
-                          uniq=True)
-    check_file(junc, os.path.join(data_folder, 'data/junc_uniq.bed'))
+    junc = fetch_juncfile(data_path('junc.bam'), uniq=True)
+    check_file(junc, data_path('junc_uniq.bed'))
     os.remove(junc)
 
 
@@ -32,16 +30,14 @@ def test_bam_to_bedgraph(data_folder):
     '''
     Testing bam_to_bedgraph()
     '''
-    bg = bam_to_bedgraph(os.path.join(data_folder, 'data/test.bam'))
-    check_file(bg, os.path.join(data_folder, 'data/unstranded.bg'))
+    bg = bam_to_bedgraph(data_path('test.bam'))
+    check_file(bg, data_path('unstranded.bg'))
     os.remove(bg)
-    bg = bam_to_bedgraph(os.path.join(data_folder, 'data/test.bam'),
-                         scale=True)
-    check_file(bg, os.path.join(data_folder, 'data/unstranded_scale.bg'))
+    bg = bam_to_bedgraph(data_path('test.bam'), scale=True)
+    check_file(bg, data_path('unstranded_scale.bg'))
     os.remove(bg)
-    pbg, mbg = bam_to_bedgraph(os.path.join(data_folder, 'data/test.bam'),
-                               stranded=True)
-    check_file(pbg, os.path.join(data_folder, 'data/plusS.bg'))
+    pbg, mbg = bam_to_bedgraph(data_path('test.bam'), stranded=True)
+    check_file(pbg, data_path('plusS.bg'))
     os.remove(pbg)
-    check_file(mbg, os.path.join(data_folder, 'data/minusS.bg'))
+    check_file(mbg, data_path('minusS.bg'))
     os.remove(mbg)
