5460b9514192a08c09febebdf3d71d3a671d8ccc
Xiao-Ou Zhang <kepbod@gmail.com>
2016-11-17 00:25:03 -0500

    fix bugs

--- seqlib/tests/test_ngs.py
+++ seqlib/tests/test_ngs.py
@@ -4,7 +4,7 @@ Testing parse.py
 
 import os
 import os.path
-from pytest.helper import data_path, check_file
+import pytest
 from seqlib.ngs import fetch_juncfile, bam_to_bedgraph
 
 
@@ -12,17 +12,19 @@ def test_fetch_juncfile():
     '''
     Testing fetch_juncfile()
     '''
-    junc = fetch_juncfile(data_path('junc.bam'))
-    check_file(junc, data_path('junc_unstranded.bed'))
+    junc = fetch_juncfile(pytest.helper.pytest.helper.data_path('junc.bam'))
+    pytest.helper.check_file(junc,
+                             pytest.helper.data_path('junc_unstranded.bed'))
     os.remove(junc)
-    junc = fetch_juncfile(data_path('junc.bam'), stranded=True)
-    check_file(junc, data_path('junc_stranded.bed'))
+    junc = fetch_juncfile(pytest.helper.data_path('junc.bam'), stranded=True)
+    pytest.helper.check_file(junc,
+                             pytest.helper.data_path('junc_stranded.bed'))
     os.remove(junc)
-    junc = fetch_juncfile(data_path('junc.bam'), min=5)
-    check_file(junc, data_path('junc_min.bed'))
+    junc = fetch_juncfile(pytest.helper.data_path('junc.bam'), min=5)
+    pytest.helper.check_file(junc, pytest.helper.data_path('junc_min.bed'))
     os.remove(junc)
-    junc = fetch_juncfile(data_path('junc.bam'), uniq=True)
-    check_file(junc, data_path('junc_uniq.bed'))
+    junc = fetch_juncfile(pytest.helper.data_path('junc.bam'), uniq=True)
+    pytest.helper.check_file(junc, pytest.helper.data_path('junc_uniq.bed'))
     os.remove(junc)
 
 
@@ -30,14 +32,16 @@ def test_bam_to_bedgraph(data_folder):
     '''
     Testing bam_to_bedgraph()
     '''
-    bg = bam_to_bedgraph(data_path('test.bam'))
-    check_file(bg, data_path('unstranded.bg'))
+    bg = bam_to_bedgraph(pytest.helper.data_path('test.bam'))
+    pytest.helper.check_file(bg, pytest.helper.data_path('unstranded.bg'))
     os.remove(bg)
-    bg = bam_to_bedgraph(data_path('test.bam'), scale=True)
-    check_file(bg, data_path('unstranded_scale.bg'))
+    bg = bam_to_bedgraph(pytest.helper.data_path('test.bam'), scale=True)
+    pytest.helper.check_file(bg,
+                             pytest.helper.data_path('unstranded_scale.bg'))
     os.remove(bg)
-    pbg, mbg = bam_to_bedgraph(data_path('test.bam'), stranded=True)
-    check_file(pbg, data_path('plusS.bg'))
+    pbg, mbg = bam_to_bedgraph(pytest.helper.data_path('test.bam'),
+                               stranded=True)
+    pytest.helper.check_file(pbg, pytest.helper.data_path('plusS.bg'))
     os.remove(pbg)
-    check_file(mbg, data_path('minusS.bg'))
+    pytest.helper.check_file(mbg, pytest.helper.data_path('minusS.bg'))
     os.remove(mbg)
